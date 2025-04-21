ce40dda38a1d06ab291ff3f0d4c2dafd23d828ac
Kumanan <kumanan1731@gmail.com>
2025-04-21 21:22:38 +0800

    Add vercel file for backend

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,4 +1,4 @@
-import { Button, Container, Image } from 'react-bootstrap';
+import { Button, Container, Image, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar';
 
@@ -7,28 +7,36 @@ const AuthPage = () => {
 		<>
 			<NavigationBar />
 
-			<Container className="d-flex flex-wrap align-items-center justify-content-between mt-100">
-				<div style={{ maxWidth: '500px' }}>
-					<h1 className="fw-bold" style={{ fontSize: '46px' }}>
-						Welcome to RecipeHub
-					</h1>
-					<p className="fst-italic" style={{ fontSize: '20px' }}>
-						Find, save, and share your favorite recipes <br />— all in one
-						place.
-					</p>
-					<Link to="/signup">
-						<Button variant="outline-primary" style={{ marginTop: '40px' }}>
-							Join Us Today
-						</Button>
-					</Link>
-				</div>
-				<div>
-					<Image
-						src="https://images.unsplash.com/photo-1490645935967-10de6ba17061?q=80&w=2053&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-						fluid
-						style={{ maxWidth: '550px', borderRadius: '12px' }}
-					/>
-				</div>
+			<Container className="mt-100">
+				<Row className="align-items-center">
+					<Col
+						xs={12}
+						md={6}
+						className="order-1 order-md-2 text-center mb-4 mb-md-0">
+						<Image
+							src="https://images.unsplash.com/photo-1490645935967-10de6ba17061?q=80&w=2053&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+							fluid
+							style={{ maxWidth: '100%', borderRadius: '12px' }}
+						/>
+					</Col>
+
+					<Col xs={12} md={6} className="order-2 order-md-1">
+						<div style={{ maxWidth: '500px' }}>
+							<h1 className="fw-bold" style={{ fontSize: '2.5rem' }}>
+								Welcome to RecipeHub
+							</h1>
+							<p className="fst-italic" style={{ fontSize: '1.25rem' }}>
+								Find, save, and share your favorite recipes <br />— all in one
+								place.
+							</p>
+							<Link to="/signup">
+								<Button variant="outline-primary" className="mt-4  w-md-auto">
+									Join Us Today
+								</Button>
+							</Link>
+						</div>
+					</Col>
+				</Row>
 			</Container>
 		</>
 	);
